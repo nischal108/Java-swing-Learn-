e926b18a874775cb63d043275d315f599af8a42e
Nischal Bhattarai <nishchalb21@gmail.com>
2024-06-17 11:08:32 +0545

    Frame Creation

---
+++ src/MyFrame.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+class FrameBuilder extends JFrame{
+
+
+
+    FrameBuilder(String name){
+        //set the name that comes with the call
+        this.setTitle(name);
+        this.setSize(400,300);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setIconImage(new ImageIcon("logo.png").getImage());
+        this.setVisible(true);
+    }
+}
+
+
+
+public class MyFrame {
+    public static void main(String[] args) {
+        //creating a frame using the constructor above
+        FrameBuilder myFrame = new FrameBuilder("Nischal Ko Frame");
+    }
+}
